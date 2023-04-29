93e6cf2fcae850a34aa29e83d86092a13643f08c
RomeandAlphaGuylol <messidtz345@gmail.com>
2023-04-29 15:34:37 +0530

    Oi

---
+++ gui.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+ 
+public class gui {
+  public static void main(String[] args){
+    JFrame frame = new JFrame();
+    frame.setSize(800,400);
+    frame.setTitle("Your GUI's Title");
+    frame.setVisible(true);
+  }
+}
